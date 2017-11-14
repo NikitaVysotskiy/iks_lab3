ba79a24f7a6e0b0aca2a81b761167793c85492ff
Slava <oneforus7@gmail.com>
2017-11-14 23:21:00 +0200

    modified version_getter script

--- get_cur_version.py
+++ get_cur_version.py
@@ -3,7 +3,6 @@ import os
 CURRENT_VERSION = os.popen('git describe --tags').read().strip()
 
 with open('version.txt', 'w') as f:
-	f.write(CURRENT_VERSION)
+    f.write(CURRENT_VERSION)
 
 print(CURRENT_VERSION)
-
