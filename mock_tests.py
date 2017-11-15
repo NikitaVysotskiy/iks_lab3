82cbdc83d19ee1d7c11b505d1cb61f81dcae0547
slava <oneforus7@gmail.com>
2017-11-15 21:18:33 +0200

    added more behavior tests

--- mock_tests.py
+++ mock_tests.py
@@ -1,4 +1,13 @@
-import unittest
+from unittest import (
+    main,
+    TestCase
+)
+from unittest.mock import (
+    MagicMock,
+    patch
+)
+
+import requests
 
 
 class Wheel:
@@ -65,7 +74,7 @@ def mock_wheel(cls):
     return cls
 
 
-class TestAuto(unittest.TestCase):
+class TestAuto(TestCase):
     def test_initial(self):
         a = Auto(Wheel)
         mocked_wheel = mock_wheel(Wheel)
@@ -106,5 +115,80 @@ class TestAuto(unittest.TestCase):
         self.assertEqual(a.get_wheels_status() + a.drive() + a.stop(),
                          a1.get_wheels_status() + a1.drive() + a1.stop(),)
 
+
+# another example
+class RequestMaker:
+    def __init__(self, user_agent=None, proxy=None):
+        self.user_agent = user_agent
+        self.proxy = proxy
+
+    def request_get(self, url):
+        r = requests.get(url,
+                         headers=self.user_agent,
+                         proxies=self.proxy)
+        return r.text
+
+
+class HtmlParse:
+    def __init__(self, html_page):
+        self.html_page = html_page
+
+    def set_html_page(self, html_page):
+        self.html_page = html_page
+
+    def parse_price(self):
+        start_pos = self.html_page.find('div.price_block')
+        value = self.html_page[start_pos:start_pos + 5]
+        return int(value.text)
+
+
+class DataSaver:
+    @staticmethod
+    def save_value(file_name, value):
+        with open(file_name, 'w') as f:
+            f.write(value)
+
+
+class ParseSystem:
+    def __init__(self, request, parser, saver):
+        self.request = request
+        self.parser = parser
+        self.saver = saver
+
+    def parse(self, url, file_name):
+        response = self.request.request_get(url)
+        self.parser.set_html_page(response)
+        value = self.parser.parse_price()
+        self.saver.save_value(file_name, value)
+
+
+class ParseSystemTestBehavior(TestCase):
+    @patch.object(RequestMaker, 'request_get', return_value='<html></html>')
+    def test_request(self, request_mock):
+        self.assertIsNotNone(request_mock('url'))
+
+    @patch.object(HtmlParse, 'set_html_page', return_value=None)
+    def test_html_page_setter(self, set_html_page_mock):
+        self.assertIsNone(set_html_page_mock('<html></html>'))
+
+    @patch.object(HtmlParse, 'parse_price', return_value=320)
+    def test_price_parsing(self, parse_price_mock):
+        self.assertGreater(parse_price_mock(), 50)
+
+    @patch.object(DataSaver, 'save_value', return_value=None)
+    def test_data_saving(self, save_value_mock):
+        self.assertIsNone(save_value_mock())
+
+
+class RequestMakerMock(TestCase):
+    @patch('mock_tests.RequestMaker')
+    def test_parsing(self, request_mock):
+        request_instance = MagicMock()
+        request_instance.request_get.return_value = '<html></html>'
+        request_mock.return_value = request_instance
+
+        self.assertIsNotNone(request_mock.request_get('url'))
+
+
 if __name__ == '__main__':
-    unittest.main()
+    main()
